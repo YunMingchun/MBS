dc4b9d61eb94ea4ee68226ba6309af111cd4f673
Ymc <1985321976@qq.com>
2015-12-22 10:52:15 +0800

    Delete Christmas -- 20151222

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var ejs = require('ejs');
+//var ejs = require('ejs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,9 +13,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-app.engine('html', ejs.__express);
-app.set('view engine', 'html');
+app.set('view engine', 'jade');
+//app.engine('html', ejs.__express);
+//app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/christmas', function(req, res, next) {
-  res.render('other/christmas', {title: 'Merry Christmas'});
-});
-
 module.exports = router;
